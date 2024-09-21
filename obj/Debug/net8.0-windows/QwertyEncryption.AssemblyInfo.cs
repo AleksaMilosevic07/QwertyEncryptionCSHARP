57d13d91c620e1ff779572c6179bf9bdd252c837
Aleksa <80622833+AleksaMilosevic07@users.noreply.github.com>
2024-09-22 00:09:04 +0200

    Polish v3

--- obj/Debug/net8.0-windows/QwertyEncryption.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/QwertyEncryption.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QwertyEncryption")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f300ec8a924bac7775fe5a09b1fad58d1945d6da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66ef9bcf64a660c93591f1af875154991a27e259")]
 [assembly: System.Reflection.AssemblyProductAttribute("QwertyEncryption")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QwertyEncryption")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
